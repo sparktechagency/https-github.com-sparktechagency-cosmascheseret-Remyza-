5612d0f8a0307aadd2ceef9242057ca4997bc687
Maruf346 <maruf.bshs@gmail.com>
2026-09-15 16:07:32 +0600

    refactor(config): update imports for static file handling

--- cheshara_config/urls.py
+++ cheshara_config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.templatetags import static
 from django.urls import path, include
-from django_extensions import settings
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
